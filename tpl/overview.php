9b530d626203deda7d0010be0e111c92d66054a8
gizmore <gizmore@wechall.net>
2017-07-02 12:55:39 +0200

    sync

--- tpl/overview.php
+++ tpl/overview.php
@@ -1,4 +1,4 @@
-<section layout="row" layout-fill flex layout-align="space-around none">
+<section layout="column" layout-gt-md="row" layout-fill flex layout-align="space-around none">
   <div flex="33"><?php echo $folders; ?></div>
   <div flex="66"><?php echo $folder; ?></div>
 </section>
